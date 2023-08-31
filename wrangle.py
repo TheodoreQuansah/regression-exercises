c6d239f0de4334fe14ad39c4efc6b2a7bac4a53f
TheodoreQuansah <theodore.s.quansah@gmail.com>
2023-08-31 10:22:06 -0500

    Scaling

--- wrangle.py
+++ wrangle.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from env import get_connection
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import QuantileTransformer, MinMaxScaler
 
 #___________________________________________________Aqcuiring the properties_2017 dataset from the zillow data base___________________________________________
 
@@ -78,9 +79,9 @@ def train_val_test(df):
     
     # Return the three datasets
     return train, val, test
+ 
 
-
-#________________________________________________________________creating a subplot______________________________________________________________
+#________________________________________________________________creating a scaled subplot______________________________________________________________
 
 def compare_data(scaled_col, x_lim, df, original='tax_value'):
     # Create a figure with two side-by-side subplots for comparison
@@ -112,3 +113,51 @@ def compare_data(scaled_col, x_lim, df, original='tax_value'):
     
     # Display the side-by-side histograms
     plt.show()
+
+
+
+#_____________________________________________________creating a quantile transformer subplot______________________________________________________________
+
+# Create a QuantileTransformer with 'normal' output distribution
+def quantiletransformer(train, output_distribution=None):
+    # Check if output_distribution is provided, if not, set it to the default value
+    if output_distribution is None:
+        output_distribution = 'uniform'  # You can choose the default you prefer
+    
+    qt = QuantileTransformer(output_distribution=output_distribution, random_state=42)
+    
+    # Fit and transform your data using the QuantileTransformer
+    train['tax_value_qt'] = qt.fit_transform(train[['tax_value']])
+    
+    # Create subplots to visualize the data before and after scaling
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+    
+    # Plot the original data
+    sns.histplot(data=train, x=train.tax_value, bins=50, ax=ax1)
+    ax1.set_title('Original data')
+    
+    # Plot the scaled data
+    sns.histplot(data=train, x=train.tax_value_qt, bins=50, ax=ax2)
+    ax2.set_title('Scaled Data (QuantileTransformer)')
+
+    plt.tight_layout()
+    plt.show()
+
+
+#_____________________________________________________creating a mms scaled function______________________________________________________________
+
+def scale(train, val, test):
+    # Create the MinMaxScaler
+    mms = MinMaxScaler()
+
+    # Fit the scaler to the original data
+    mms.fit(train[['squarefeet', 'tax_value', 'tax_amount']])
+    mms.fit(val[['squarefeet', 'tax_value', 'tax_amount']])
+    mms.fit(test[['squarefeet', 'tax_value', 'tax_amount']])
+    
+    # Transform the entire original data (all columns)
+    train[['squarefeet', 'tax_value', 'tax_amount']] = mms.transform(train[['squarefeet', 'tax_value', 'tax_amount']])
+    val[['squarefeet', 'tax_value', 'tax_amount']] = mms.transform(val[['squarefeet', 'tax_value', 'tax_amount']])
+    test[['squarefeet', 'tax_value', 'tax_amount']] = mms.transform(test[['squarefeet', 'tax_value', 'tax_amount']])
+
+    return train, val, test
